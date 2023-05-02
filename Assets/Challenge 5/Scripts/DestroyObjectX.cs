49b3ae6ad21d53c5aebd34b476713256cbfec5e6
ssmart24 <ssmart24@aoiths.org>
2023-05-02 11:59:48 -0400

    Challenge 5

---
+++ Assets/Challenge 5/Scripts/DestroyObjectX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjectX : MonoBehaviour
+{
+    private GameManagerX gmx;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+
+
+    void Start()
+    {
+       
+    }
+    private void OnMouseDown()
+    {
+        if (gmx.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gmx.UpdateScore(pointValue);
+        }
+       
+    }
+
+}
